dc9129c6225fc012893cec54b1bc19d041257696
LilStumpie <ivanbur228@gmail.com>
2025-12-06 10:58:54 +0300

    fixed

--- 5lab/third.cpp
+++ 5lab/third.cpp
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <math.h>
 
+typedef double (*func_ptr_t)(double);
+
+
+double func1(double x) {
+    return 1 / sqrt(5 - pow(x, 3));
+}
+
+double func2(double x) {
+    return 1 / (3 * pow(x, 2) - 2 * x + 4);
+}
+
 int select_function() {
     int a;
     printf("Выберите функцию, которую надо посчитать: (1 или 2): ");
@@ -8,49 +19,50 @@ int select_function() {
     return a;
 }
 
-double func(double x, int choice) {
-    if (choice == 1) {
-        return 1 / sqrt(5 - pow(x, 3));
-    } else if (choice == 2) {
-        return 1 / (3 * pow(x, 2) - 2 * x + 4);
+double parabola(int n, func_ptr_t func) {
+    double a, b, h, sum_odd = 0.0, sum_even = 0.0;
+
+    if (func == func1) {
+        a = -2;
+        b = 1;
+    } else if (func == func2) {
+        a = -1;
+        b = 3;
     } else {
         return 0;
     }
-}
 
-double parabola(int n, int choice) {
-    double a;
-    double b;
-    if(choice == 1){
-       a = -2;
-       b = 1;
-    };
-    if(choice == 2){
-       a = -1;
-       b = 3;
-    };
-    double h = (b - a) / n;
-    double sum_odd = 0.0, sum_even = 0.0;
+    h = (b - a) / n;
 
     for (int i = 1; i < n; i++) {
         double x = a + i * h;
         if (i % 2 == 0) {
-            sum_even += func(x, choice);
+            sum_even += func(x);
         } else {
-            sum_odd += func(x, choice);
+            sum_odd += func(x);
         }
     }
 
-    double integral = (h/3) * (func(a, choice) + 4 * sum_odd + 2 * sum_even + func(b, choice));
+    double integral = (h / 3) * (func(a) + 4 * sum_odd + 2 * sum_even + func(b));
     return integral;
 }
 
 int main() {
     int choice = select_function();
+    func_ptr_t func;
+    if (choice == 1) {
+        func = func1;
+    } else if (choice == 2) {
+        func = func2;
+    } else {
+        printf("Некорректный выбор функции.\n");
+        return 1;
+    }
+
     int n = 10;
 
-    double result = parabola(n, choice);
-    printf("Приблизительный значение интеграла: %.8f\n", result);
+    double result = parabola(n, func);
+    printf("Приблизительное значение интеграла: %.8f\n", result);
 
     return 0;
 }
